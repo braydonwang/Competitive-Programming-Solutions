acf1f6e044c391a010818527f41f9fda808f4df8
Braydon Wang <braydon.wang@gmail.com>
2021-07-23 15:17:57 -0400

    Create A.java

---
+++ Codeforces/Codeforces Global Round 10/A.java
@@ -0,0 +1,42 @@
+import java.util.*;
+import java.io.*;
+public class CCC2 {
+	static BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+	static StringTokenizer st;
+	public static void main(String[] args) throws IOException {
+		int t = readInt();
+		for (int i = 0; i < t; i++) {
+			int n = readInt();
+			int[] array = new int[n];
+			for (int j = 0; j < n; j++) {
+				array[j] = readInt();
+			}
+			Arrays.sort(array);
+			if (array[0] == array[n-1]) {
+				System.out.println(n);
+			} else {
+				System.out.println(1);
+			}
+		}
+	}
+	static String next () throws IOException {
+		while (st == null || !st.hasMoreTokens())
+			st = new StringTokenizer(br.readLine().trim());
+		return st.nextToken();
+	}
+	static long readLong () throws IOException {
+		return Long.parseLong(next());
+	}
+	static int readInt () throws IOException {
+		return Integer.parseInt(next());
+	}
+	static double readDouble () throws IOException {
+		return Double.parseDouble(next());
+	}
+	static char readCharacter () throws IOException {
+		return next().charAt(0);
+	}
+	static String readLine () throws IOException {
+		return br.readLine().trim();
+	}
+}
