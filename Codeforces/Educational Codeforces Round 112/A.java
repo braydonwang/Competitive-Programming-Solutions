ddaa20123e791f2f858e0516ed346fe447433d79
Braydon Wang <braydon.wang@gmail.com>
2021-07-30 15:59:21 -0400

    Create A.java

---
+++ Codeforces/Educational Codeforces Round 112/A.java
@@ -0,0 +1,40 @@
+import java.util.*;
+import java.io.*;
+public class Main {
+	static BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+	static StringTokenizer st;
+	public static void main(String[] args) throws IOException {
+		int t = readInt();
+		for (int i = 0; i < t; i++) {
+			long n = readLong();
+			if (n <= 6) {
+				System.out.println(15);
+				continue;
+			}
+			if (n % 2 != 0) {
+				n++;
+			}
+			System.out.println((long)((n*5)/2));
+		}
+	}
+	static String next () throws IOException {
+		while (st == null || !st.hasMoreTokens())
+			st = new StringTokenizer(br.readLine().trim());
+		return st.nextToken();
+	}
+	static long readLong () throws IOException {
+		return Long.parseLong(next());
+	}
+	static int readInt () throws IOException {
+		return Integer.parseInt(next());
+	}
+	static double readDouble () throws IOException {
+		return Double.parseDouble(next());
+	}
+	static char readCharacter () throws IOException {
+		return next().charAt(0);
+	}
+	static String readLine () throws IOException {
+		return br.readLine().trim();
+	}
+}
