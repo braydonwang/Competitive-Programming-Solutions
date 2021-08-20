e18ceb21c0e204771253452217c0de26af591b08
Braydon Wang <braydon.wang@gmail.com>
2021-08-20 11:34:04 -0400

    Create phantom1.java

---
+++ DMOJ/Java/phantom1.java
@@ -0,0 +1,50 @@
+import java.util.*;
+import java.io.*;
+public class CCC2 {
+	static BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+	static StringTokenizer st;
+	public static void main(String[] args) throws IOException {
+		int n = readInt();
+		for (int i = 0; i < n; i++) {
+			int count = 0;
+			int a = readInt(), b = readInt();
+			for (int j = a; j < b; j++) {
+				if (isPrime(j)) {
+					count++;
+				}
+			}
+			System.out.println(count);
+		}
+	}
+	static boolean isPrime(int x) {
+		if (x == 1) {
+			return false;
+		}
+		for (int i = 2; i <= Math.sqrt(x); i++) {
+			if (x % i == 0) {
+				return false;
+			}
+		}
+		return true;
+	}
+	static String next () throws IOException {
+		while (st == null || !st.hasMoreTokens())
+			st = new StringTokenizer(br.readLine().trim());
+		return st.nextToken();
+	}
+	static long readLong () throws IOException {
+		return Long.parseLong(next());
+	}
+	static int readInt () throws IOException {
+		return Integer.parseInt(next());
+	}
+	static double readDouble () throws IOException {
+		return Double.parseDouble(next());
+	}
+	static char readCharacter () throws IOException {
+		return next().charAt(0);
+	}
+	static String readLine () throws IOException {
+		return br.readLine().trim();
+	}
+}
