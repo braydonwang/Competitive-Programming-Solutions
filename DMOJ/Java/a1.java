f88436bd2ffca1cecf84a39956b3877c9c3dfa25
Braydon Wang <braydon.wang@gmail.com>
2021-08-20 15:14:09 -0400

    Create a1.java

---
+++ DMOJ/Java/a1.java
@@ -0,0 +1,35 @@
+import java.util.*;
+import java.io.*;
+public class CCC2 {
+	static BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+	static StringTokenizer st;
+	public static void main(String[] args) throws IOException {
+		int n = readInt();
+		for (int i = 1; i <= n; i++) {
+			String str = readLine();
+			int x = Integer.parseInt(str.substring(0,str.indexOf(" ")));
+			String temp = str.substring(str.indexOf(" ") + 1);
+			System.out.println(i + " " + temp.substring(0,x-1) + temp.substring(x));
+		}
+	}
+	static String next () throws IOException {
+		while (st == null || !st.hasMoreTokens())
+			st = new StringTokenizer(br.readLine().trim());
+		return st.nextToken();
+	}
+	static long readLong () throws IOException {
+		return Long.parseLong(next());
+	}
+	static int readInt () throws IOException {
+		return Integer.parseInt(next());
+	}
+	static double readDouble () throws IOException {
+		return Double.parseDouble(next());
+	}
+	static char readCharacter () throws IOException {
+		return next().charAt(0);
+	}
+	static String readLine () throws IOException {
+		return br.readLine().trim();
+	}
+}
