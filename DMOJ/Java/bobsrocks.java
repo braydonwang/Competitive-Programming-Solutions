699d705ad44d73fa61b3d922efbc29a0ebdf3186
Braydon Wang <braydon.wang@gmail.com>
2021-08-20 15:16:53 -0400

    Create bobsrocks.java

---
+++ DMOJ/Java/bobsrocks.java
@@ -0,0 +1,40 @@
+import java.util.*;
+import java.io.*;
+public class Template {
+	static BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+	static StringTokenizer st;
+	public static void main(String[] args) throws IOException {
+		int n = readInt();
+		int[] a = new int[51];
+		int max = 0, ans = 0;
+		for (int i = 0; i < n; i++) {
+			int x = readInt();
+			a[x]++;
+			if (a[x] * x > max) {
+				max = a[x] * x;
+				ans = x;
+			}
+		}
+		System.out.println(ans);
+	}
+	static String next () throws IOException {
+		while (st == null || !st.hasMoreTokens())
+			st = new StringTokenizer(br.readLine().trim());
+		return st.nextToken();
+	}
+	static long readLong () throws IOException {
+		return Long.parseLong(next());
+	}
+	static int readInt () throws IOException {
+		return Integer.parseInt(next());
+	}
+	static double readDouble () throws IOException {
+		return Double.parseDouble(next());
+	}
+	static char readCharacter () throws IOException {
+		return next().charAt(0);
+	}
+	static String readLine () throws IOException {
+		return br.readLine().trim();
+	}
+}
