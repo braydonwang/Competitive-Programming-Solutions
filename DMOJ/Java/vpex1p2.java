e00e4161ec8f7889c2bd12158e43af0b7fae0e88
Braydon Wang <braydon.wang@gmail.com>
2021-08-20 12:42:33 -0400

    Create vpex1p2.java

---
+++ DMOJ/Java/vpex1p2.java
@@ -0,0 +1,43 @@
+import java.util.*;
+import java.io.*;
+public class Template {
+	static BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+	static StringTokenizer st;
+	public static void main(String[] args) throws IOException {
+		int n = readInt();
+		int[] array = new int[n];
+		int total = 0;
+		for (int i = 0; i < n; i++) {
+			array[i] = readInt();
+			total += array[i];
+		}
+		total = total / n;
+		int count = 0;
+		for (int i = 0; i < n; i++) {
+			if (array[i] != total) {
+				count++;
+			}
+		}
+		System.out.println(count);
+	}
+	static String next () throws IOException {
+		while (st == null || !st.hasMoreTokens())
+			st = new StringTokenizer(br.readLine().trim());
+		return st.nextToken();
+	}
+	static long readLong () throws IOException {
+		return Long.parseLong(next());
+	}
+	static int readInt () throws IOException {
+		return Integer.parseInt(next());
+	}
+	static double readDouble () throws IOException {
+		return Double.parseDouble(next());
+	}
+	static char readCharacter () throws IOException {
+		return next().charAt(0);
+	}
+	static String readLine () throws IOException {
+		return br.readLine().trim();
+	}
+}
