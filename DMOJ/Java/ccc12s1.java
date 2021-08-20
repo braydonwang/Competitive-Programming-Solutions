69e2d7f3ffa1016c272392c1280c42bc93fb2e21
Braydon Wang <braydon.wang@gmail.com>
2021-08-20 15:51:35 -0400

    Create ccc12s1.java

---
+++ DMOJ/Java/ccc12s1.java
@@ -0,0 +1,38 @@
+import java.util.*;
+import java.io.*;
+public class Main {
+	static BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+	static StringTokenizer st;
+	public static void main(String[] args) throws IOException {
+		int n = readInt();
+		int count = 0;
+		for (int i = 1; i <= n-3; i++) {
+			for (int j = i+1; j <= n-2; j++) {
+				for (int k = j+1; k <= n-1; k++) {
+					count++;
+				}
+			}
+		}
+		System.out.println(count);
+	}
+	static String next () throws IOException {
+		while (st == null || !st.hasMoreTokens())
+			st = new StringTokenizer(br.readLine().trim());
+		return st.nextToken();
+	}
+	static long readLong () throws IOException {
+		return Long.parseLong(next());
+	}
+	static int readInt () throws IOException {
+		return Integer.parseInt(next());
+	}
+	static double readDouble () throws IOException {
+		return Double.parseDouble(next());
+	}
+	static char readCharacter () throws IOException {
+		return next().charAt(0);
+	}
+	static String readLine () throws IOException {
+		return br.readLine().trim();
+	}
+}
