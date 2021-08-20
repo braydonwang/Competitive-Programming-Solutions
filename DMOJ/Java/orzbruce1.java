3e4296345b90714f2c1b21bf1e46f3966d8a8972
Braydon Wang <braydon.wang@gmail.com>
2021-08-20 15:17:45 -0400

    Create orzbruce1.java

---
+++ DMOJ/Java/orzbruce1.java
@@ -0,0 +1,31 @@
+import java.util.*;
+import java.io.*;
+public class Main {
+	static BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+	static StringTokenizer st;
+	public static void main(String[] args) throws IOException {
+		int a = readInt(), b = readInt(), c = readInt();
+		double ans = (a/40000.0 + b/60000.0 + c/70000.0) * 60.0;
+		System.out.printf("%.2f", ans);
+	}
+	static String next () throws IOException {
+		while (st == null || !st.hasMoreTokens())
+			st = new StringTokenizer(br.readLine().trim());
+		return st.nextToken();
+	}
+	static long readLong () throws IOException {
+		return Long.parseLong(next());
+	}
+	static int readInt () throws IOException {
+		return Integer.parseInt(next());
+	}
+	static double readDouble () throws IOException {
+		return Double.parseDouble(next());
+	}
+	static char readCharacter () throws IOException {
+		return next().charAt(0);
+	}
+	static String readLine () throws IOException {
+		return br.readLine().trim();
+	}
+}
