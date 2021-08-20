e841d6fa6bbcb68786e1f35726390138ad984624
Braydon Wang <braydon.wang@gmail.com>
2021-08-20 12:48:45 -0400

    Create wac5p1.java

---
+++ DMOJ/Java/wac5p1.java
@@ -0,0 +1,46 @@
+import java.util.*;
+import java.io.*;
+public class CCC2 {
+	static BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+	static StringTokenizer st;
+	public static void main(String[] args) throws IOException {
+		int n = readInt();
+		int[] array = new int[n];
+		for (int i = 0; i < n; i++) {
+			array[i] = readInt();
+		}
+		Arrays.sort(array);
+		int ans = array[n-1], count = 1, total = 1;
+		for (int i = n-2; i >= 0; i--) {
+			if (array[i] == array[i+1]) {
+				count++;
+				if (count > total) {
+					ans += array[i]; total++;
+				}
+			} else {
+				count = 1;
+			}
+		}
+		System.out.println(ans);
+	}
+	static String next () throws IOException {
+		while (st == null || !st.hasMoreTokens())
+			st = new StringTokenizer(br.readLine().trim());
+		return st.nextToken();
+	}
+	static long readLong () throws IOException {
+		return Long.parseLong(next());
+	}
+	static int readInt () throws IOException {
+		return Integer.parseInt(next());
+	}
+	static double readDouble () throws IOException {
+		return Double.parseDouble(next());
+	}
+	static char readCharacter () throws IOException {
+		return next().charAt(0);
+	}
+	static String readLine () throws IOException {
+		return br.readLine().trim();
+	}
+}
