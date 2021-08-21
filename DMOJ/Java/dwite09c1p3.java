e984ca3ab227c0924a7bef2d1b2a04e9c7511fd4
Braydon Wang <braydon.wang@gmail.com>
2021-08-21 11:59:44 -0400

    Create dwite09c1p3.java

---
+++ DMOJ/Java/dwite09c1p3.java
@@ -0,0 +1,16 @@
+import java.io.*;
+import java.util.*;
+
+public class Main {
+    public static void main(String[] args) {
+        Scanner sc = new Scanner(System.in);
+        for (int a = 0; a < 5; a++){
+            int n = sc.nextInt();
+            int sum = ((n+1)*(n+2))/2;
+            for (int i = 0; i < n; i++){
+                sum -= sc.nextInt();
+            }
+            System.out.println(sum);
+        }
+    }
+}
