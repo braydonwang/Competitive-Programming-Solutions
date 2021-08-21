bfbaeae175ff3c24912828fbe552980e38b29022
Braydon Wang <braydon.wang@gmail.com>
2021-08-21 12:12:39 -0400

    Create a4b1.java

---
+++ DMOJ/Java/a4b1.java
@@ -0,0 +1,37 @@
+import java.util.*;
+import java.io.*;
+public class CCC2 {
+	static BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+	static StringTokenizer st;
+	public static void main(String[] args) throws IOException {
+		int n = readInt();
+		int[] array = new int[n];
+		for (int i = 0; i < n; i++) {
+			array[i] = readInt();
+		}
+		Arrays.sort(array);
+		for (int i = 0; i < n; i++) {
+			System.out.println(array[i]);
+		}
+	}
+	static int readInt () throws IOException {
+		return Integer.parseInt(next());
+	}
+	static String next () throws IOException {
+		while (st == null || !st.hasMoreTokens())
+			st = new StringTokenizer(br.readLine().trim());
+		return st.nextToken();
+	}
+	static long readLong () throws IOException {
+		return Long.parseLong(next());
+	}
+	static double readDouble () throws IOException {
+		return Double.parseDouble(next());
+	}
+	static char readCharacter () throws IOException {
+		return next().charAt(0);
+	}
+	static String readLine () throws IOException {
+		return br.readLine().trim();
+	}
+}
