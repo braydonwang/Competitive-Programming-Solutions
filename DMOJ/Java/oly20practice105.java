b787e3e2006b6b828da30fd2031af1023ace56e9
Braydon Wang <braydon.wang@gmail.com>
2021-08-21 14:32:04 -0400

    Create oly20practice105.java

---
+++ DMOJ/Java/oly20practice105.java
@@ -0,0 +1,31 @@
+import java.util.*;
+import java.io.*;
+public class CCC2 {
+	static BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+	static StringTokenizer st;
+	public static void main(String[] args) throws IOException {
+		long n = readInt(), m = readInt();
+		long total = n*m - (n+m-1);
+		System.out.println(total);
+	}
+	static String next () throws IOException {
+		while (st == null || !st.hasMoreTokens())
+			st = new StringTokenizer(br.readLine().trim());
+		return st.nextToken();
+	}
+	static long readLong () throws IOException {
+		return Long.parseLong(next());
+	}
+	static int readInt () throws IOException {
+		return Integer.parseInt(next());
+	}
+	static double readDouble () throws IOException {
+		return Double.parseDouble(next());
+	}
+	static char readCharacter () throws IOException {
+		return next().charAt(0);
+	}
+	static String readLine () throws IOException {
+		return br.readLine().trim();
+	}
+}
