833ab6dbbf01fc49939b28791b8232277af0cc09
Braydon Wang <braydon.wang@gmail.com>
2021-08-20 21:23:25 -0400

    Create dmopc17c1p1.java

---
+++ DMOJ/Java/dmopc17c1p1.java
@@ -0,0 +1,50 @@
+import java.util.*;
+import java.io.*;
+public class CCC2 {
+	static BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+	static StringTokenizer st;
+	public static void main(String[] args) throws IOException {
+		int r = readInt(), c = readInt();
+		ArrayList<Integer> x = new ArrayList();
+		ArrayList<Integer> y = new ArrayList();
+		for (int i = 0; i < r; i++) {
+			String str = next();
+			for (int j = 0; j < c; j++) {
+				if (str.charAt(j) == 'X') {
+					x.add(j); y.add(i);
+				}
+			}
+		}
+		int q = readInt();
+		for (int i = 0; i < q; i++) {
+			int a = readInt()-1, b = readInt()-1;
+			if (x.contains(a)) {
+				System.out.println("Y");
+			} else if (y.contains(b)) {
+				System.out.println("Y");
+			} else {
+				System.out.println("N");
+			}
+		}
+	}
+	static String next () throws IOException {
+		while (st == null || !st.hasMoreTokens())
+			st = new StringTokenizer(br.readLine().trim());
+		return st.nextToken();
+	}
+	static long readLong () throws IOException {
+		return Long.parseLong(next());
+	}
+	static int readInt () throws IOException {
+		return Integer.parseInt(next());
+	}
+	static double readDouble () throws IOException {
+		return Double.parseDouble(next());
+	}
+	static char readCharacter () throws IOException {
+		return next().charAt(0);
+	}
+	static String readLine () throws IOException {
+		return br.readLine().trim();
+	}
+}
