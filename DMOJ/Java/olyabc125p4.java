f53ff3a4a7afc40d6a4c8280e640ab243b4f5953
Braydon Wang <braydon.wang@gmail.com>
2021-08-21 14:16:05 -0400

    Create olyabc125p4.java

---
+++ DMOJ/Java/olyabc125p4.java
@@ -0,0 +1,48 @@
+import java.util.*;
+import java.io.*;
+public class CCC2 {
+	static BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+	static StringTokenizer st;
+	public static void main(String[] args) throws IOException {
+		int n = readInt();
+		int count = 0;
+		int[] array = new int[n];
+		long sum = 0;
+		for (int i = 0; i < n; i++) {
+			int x = readInt();
+			if (x < 0) {
+				count++;
+				array[i] = -x;
+			} else {
+				array[i] = x;
+			}
+			sum += array[i];
+		}
+		if (count % 2 == 0) {
+			System.out.println(sum);
+		} else {
+			Arrays.sort(array);
+			System.out.println(sum - array[0] - array[0]);
+		}
+	}
+	static String next () throws IOException {
+		while (st == null || !st.hasMoreTokens())
+			st = new StringTokenizer(br.readLine().trim());
+		return st.nextToken();
+	}
+	static long readLong () throws IOException {
+		return Long.parseLong(next());
+	}
+	static int readInt () throws IOException {
+		return Integer.parseInt(next());
+	}
+	static double readDouble () throws IOException {
+		return Double.parseDouble(next());
+	}
+	static char readCharacter () throws IOException {
+		return next().charAt(0);
+	}
+	static String readLine () throws IOException {
+		return br.readLine().trim();
+	}
+}
