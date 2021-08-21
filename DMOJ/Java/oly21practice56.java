e06f792b68966d7b2ed63bb5f443bf01dbe9e2fe
Braydon Wang <braydon.wang@gmail.com>
2021-08-21 17:35:30 -0400

    Create oly21practice56.java

---
+++ DMOJ/Java/oly21practice56.java
@@ -0,0 +1,35 @@
+import java.util.*;
+import java.io.*;
+public class Main {
+	static BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+	static StringTokenizer st;
+	public static void main(String[] args) throws IOException {
+		long x = readInt(), y = readInt();
+		long lcm = x*y / gcd(x,y);
+		System.out.println(lcm);
+	}
+	static long gcd(long a, long b) {
+		if (b == 0) return a;
+		return gcd(b, a % b);
+	}
+	static String next () throws IOException {
+		while (st == null || !st.hasMoreTokens())
+			st = new StringTokenizer(br.readLine().trim());
+		return st.nextToken();
+	}
+	static long readLong () throws IOException {
+		return Long.parseLong(next());
+	}
+	static int readInt () throws IOException {
+		return Integer.parseInt(next());
+	}
+	static double readDouble () throws IOException {
+		return Double.parseDouble(next());
+	}
+	static char readCharacter () throws IOException {
+		return next().charAt(0);
+	}
+	static String readLine () throws IOException {
+		return br.readLine().trim();
+	}
+}
