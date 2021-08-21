46455079bc8011437e0133b7cb97ccb44cd37a5a
Braydon Wang <braydon.wang@gmail.com>
2021-08-21 11:40:24 -0400

    Create dmopc20c1p1.java

---
+++ DMOJ/Java/dmopc20c1p1.java
@@ -0,0 +1,40 @@
+import java.util.*;
+import java.io.*;
+import java.lang.reflect.Array;
+public class CCC2 {
+	static BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+	static StringTokenizer st;
+	public static void main(String[] args) throws IOException {
+		int n = readInt();
+		for (int i = 0; i < n; i++) {
+			String str = next();
+			if (str.contains("M") && str.contains("C")) {
+				System.out.println("NEGATIVE MARKS");
+			} else if (str.contains("M") || str.contains("C")) {
+				System.out.println("FAIL");
+			} else {
+				System.out.println("PASS");
+			}
+		}
+	}
+	static int readInt () throws IOException {
+		return Integer.parseInt(next());
+	}
+	static String next () throws IOException {
+		while (st == null || !st.hasMoreTokens())
+			st = new StringTokenizer(br.readLine().trim());
+		return st.nextToken();
+	}
+	static long readLong () throws IOException {
+		return Long.parseLong(next());
+	}
+	static double readDouble () throws IOException {
+		return Double.parseDouble(next());
+	}
+	static char readCharacter () throws IOException {
+		return next().charAt(0);
+	}
+	static String readLine () throws IOException {
+		return br.readLine().trim();
+	}
+}
