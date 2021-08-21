fc8f58294fdd35dba4c7dfc14762b8ab70aab8a1
Braydon Wang <braydon.wang@gmail.com>
2021-08-21 17:30:55 -0400

    Create oly20practice134.java

---
+++ DMOJ/Java/oly20practice134.java
@@ -0,0 +1,43 @@
+import java.util.*;
+import java.io.*;
+public class CCC2 {
+	static BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+	static StringTokenizer st;
+	public static void main(String[] args) throws IOException {
+		int n = readInt();
+		int[] a = new int[n];
+		for (int i = 0; i < n; i++) {
+			a[i] = readInt();
+		}
+		int max = 1, count = 1;
+		for (int i = 1; i < n; i++) {
+			if (a[i] <= a[i-1]) {
+				count++;
+			} else {
+				count = 1;
+			}
+			max = Math.max(max, count);
+		}
+		System.out.println(max);
+	}
+	static String next () throws IOException {
+		while (st == null || !st.hasMoreTokens())
+			st = new StringTokenizer(br.readLine().trim());
+		return st.nextToken();
+	}
+	static long readLong () throws IOException {
+		return Long.parseLong(next());
+	}
+	static int readInt () throws IOException {
+		return Integer.parseInt(next());
+	}
+	static double readDouble () throws IOException {
+		return Double.parseDouble(next());
+	}
+	static char readCharacter () throws IOException {
+		return next().charAt(0);
+	}
+	static String readLine () throws IOException {
+		return br.readLine().trim();
+	}
+}
