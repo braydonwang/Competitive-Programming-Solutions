8868c540ad99e21f2b5e6da81476e5a8543b3d58
Braydon Wang <braydon.wang@gmail.com>
2021-08-21 14:15:11 -0400

    Create oly18novp6.java

---
+++ DMOJ/Java/oly18novp6.java
@@ -0,0 +1,287 @@
+import java.util.*;
+import java.io.*;
+public class CCC2 {
+	static BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+	static StringTokenizer st;
+	static int[] a;
+	static Node[] tree;
+	public static void main(String[] args) throws IOException {
+		int n = readInt();
+		tree = new Node[(int)2e6];
+		a = new int[n+1];
+		ArrayList<Integer> list = new ArrayList();
+		for (int i = 1; i <= n; i++) {
+			a[i] = readInt();
+			if (a[i] > 1) {
+				list.add(i);
+			}
+		}
+		//Collections.sort(list);
+		build(1,1,n);
+		int m = readInt();
+		for (int i = 0; i < m; i++) {
+			int x = readInt(), l = readInt(), r = readInt();
+			if (x == 1) {
+				System.out.println(query(1,l,r));
+			} else {
+				int left = l;
+				while (true) {
+					int lo = 0, hi = list.size()-1, ans = -1, idx = -1;
+					while (lo <= hi) {
+						int mid = (lo + hi) / 2;
+						int temp = list.get(mid);
+						if (temp >= left) {
+							ans = temp; idx = mid;
+							hi = mid - 1;
+						} else {
+							lo = mid + 1;
+						}
+					}
+					if (ans > r || ans == -1) {
+						break;
+					}
+					update(1,ans,(int)Math.sqrt(a[ans])-a[ans]);
+					//System.out.println((int)Math.sqrt(a[ans])-a[ans]);
+					a[ans] = (int)Math.sqrt(a[ans]);
+					
+					if (a[ans] == 1) {
+						list.remove(idx);
+					}
+					left = ans + 1;
+				}
+				//System.out.println(Arrays.toString(a));
+			}
+		}
+	}
+	static long build(int pos, int l, int r) {
+		tree[pos] = new Node(l,r,0);
+		if (l == r) {
+			tree[pos].val = a[l];
+			return tree[pos].val;
+		}
+		int mid = (l + r) / 2;
+		return tree[pos].val = build(pos*2,l,mid) + build(pos*2+1,mid+1,r);
+	}
+	static long update(int pos, int ind, int x) {
+		int l = tree[pos].l, r = tree[pos].r;
+		if (l > ind || r < ind) {
+			return tree[pos].val;
+		} else if (l == ind && r == ind) {
+			tree[pos].val += x;
+			return tree[pos].val;
+		} else if (l <= ind && r >= ind) {
+			tree[pos].val -= x;
+		}
+		return tree[pos].val = update(pos*2,ind,x) + update(pos*2+1,ind,x);
+	}
+	static long query(int pos, int ll, int rr) {
+		int l = tree[pos].l, r = tree[pos].r;
+		if (l > rr || r < ll) {
+			return 0;
+		} else if (l >= ll && r <= rr) {
+			return tree[pos].val;
+		}
+		return query(pos*2,ll,rr) + query(pos*2+1,ll,rr);
+	}
+	static class Node{
+		int l, r;
+		long val;
+		Node(int l, int r, long val){
+			this.l = l;
+			this.r = r;
+			this.val = val;
+		}
+	}
+		private static final int BUFFER_SIZE = 1 << 16;
+	private static final InputStream in = System.in;
+	private static final byte[] buffer = new byte[BUFFER_SIZE];
+	private static int bufferPointer = 0, bytesRead = 0;
+
+	public static String read() throws IOException {
+		byte[] ret = new byte[1024];
+		int idx = 0;
+		byte c = Read();
+		while (c <= ' ') {
+			c = Read();
+		}
+		do {
+			ret[idx++] = c;
+			c = Read();
+		} while (c != -1 && c != ' ' && c != '\n' && c != '\r');
+		return new String(ret, 0, idx);
+	}
+
+	public static String readLine() throws IOException {
+		final StringBuilder sb = new StringBuilder(1024);
+		byte c = Read();
+		while (c == '\n' || c == '\r') {
+			c = Read();
+		}
+		do {
+			sb.append((char) c);
+			c = Read();
+		} while (c != -1 && c != '\n' && c != '\r');
+		return sb.toString();
+	}
+
+	public static char readChar() throws IOException {
+		byte c = Read();
+		while (Character.isWhitespace(c)) {
+			c = Read();
+		}
+		return (char) c;
+	}
+
+	private static char[] readCharArray(final int chars) throws IOException {
+		final char[] arr = new char[chars];
+		for (int i = 0; i < chars; i++) {
+			arr[i] = readChar();
+		}
+		return arr;
+	}
+
+	public static java.math.BigInteger readBigInteger() throws IOException {
+		return new java.math.BigInteger(read());
+	}
+
+	public static java.math.BigDecimal readBigDecimal() throws IOException {
+		return new java.math.BigDecimal(read());
+	}
+
+	public static byte readByte() throws IOException {
+		byte ret = 0;
+		byte c = Read();
+		while (c <= ' ')
+			c = Read();
+		boolean neg = (c == '-');
+		if (neg)
+			c = Read();
+		do {
+			ret = (byte) (ret * 10 + c - '0');
+		} while ((c = Read()) >= '0' && c <= '9');
+		if (neg)
+			return (byte) -ret;
+		return ret;
+	}
+
+	public static byte[] readByteArray(final int numbers) throws IOException {
+		final byte[] arr = new byte[numbers];
+		for (int i = 0; i < numbers; i++) {
+			arr[i] = readByte();
+		}
+		return arr;
+	}
+
+	public static short readShort() throws IOException {
+		short ret = 0;
+		byte c = Read();
+		while (c <= ' ')
+			c = Read();
+		boolean neg = (c == '-');
+		if (neg)
+			c = Read();
+		do {
+			ret = (short) (ret * 10 + c - '0');
+		} while ((c = Read()) >= '0' && c <= '9');
+		if (neg)
+			return (short) -ret;
+		return ret;
+	}
+
+	public static short[] readShortArray(final int numbers) throws IOException {
+		final short[] arr = new short[numbers];
+		for (int i = 0; i < numbers; i++) {
+			arr[i] = readShort();
+		}
+		return arr;
+	}
+
+	public static int readInt() throws IOException {
+		int ret = 0;
+		byte c = Read();
+		while (c <= ' ')
+			c = Read();
+		boolean neg = (c == '-');
+		if (neg)
+			c = Read();
+		do {
+			ret = ret * 10 + c - '0';
+		} while ((c = Read()) >= '0' && c <= '9');
+		if (neg)
+			return -ret;
+		return ret;
+	}
+
+	public static int[] readIntArray(final int numbers) throws IOException {
+		final int[] arr = new int[numbers];
+		for (int i = 0; i < numbers; i++) {
+			arr[i] = readInt();
+		}
+		return arr;
+	}
+
+	public static long readLong() throws IOException {
+		long ret = 0;
+		byte c = Read();
+		while (c <= ' ')
+			c = Read();
+		boolean neg = (c == '-');
+		if (neg)
+			c = Read();
+		do {
+			ret = ret * 10 + c - '0';
+		} while ((c = Read()) >= '0' && c <= '9');
+		if (neg)
+			return -ret;
+		return ret;
+	}
+
+	public static long[] readLongArray(final int numbers) throws IOException {
+		final long[] arr = new long[numbers];
+		for (int i = 0; i < numbers; i++) {
+			arr[i] = readLong();
+		}
+		return arr;
+	}
+
+	public static double readDouble() throws IOException {
+		double ret = 0, div = 1;
+		byte c = Read();
+		while (c <= ' ')
+			c = Read();
+		boolean neg = (c == '-');
+		if (neg)
+			c = Read();
+		do {
+			ret = ret * 10 + c - '0';
+		} while ((c = Read()) >= '0' && c <= '9');
+		if (c == '.') {
+			while ((c = Read()) >= '0' && c <= '9') {
+				ret += (c - '0') / (div *= 10);
+			}
+		}
+		if (neg)
+			return -ret;
+		return ret;
+	}
+
+	public static double[] readDoubleArray(final int numbers) throws IOException {
+		final double[] arr = new double[numbers];
+		for (int i = 0; i < numbers; i++) {
+			arr[i] = readDouble();
+		}
+		return arr;
+	}
+
+	private static void fillBuffer() throws IOException {
+		bytesRead = in.read(buffer, bufferPointer = 0, BUFFER_SIZE);
+		if (bytesRead == -1)
+			buffer[0] = -1;
+	}
+
+	private static byte Read() throws IOException {
+		if (bufferPointer == bytesRead)
+			fillBuffer();
+		return buffer[bufferPointer++];
+	}
+}
