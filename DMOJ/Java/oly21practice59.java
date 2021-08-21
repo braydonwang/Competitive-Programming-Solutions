3e8fa83f513776e60e350fe3428ef8c131077417
Braydon Wang <braydon.wang@gmail.com>
2021-08-21 17:39:09 -0400

    Create oly21practice59.java

---
+++ DMOJ/Java/oly21practice59.java
@@ -0,0 +1,41 @@
+import java.util.*;
+import java.io.*;
+public class Main {
+	static BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+	static StringTokenizer st;
+	public static void main(String[] args) throws IOException {
+		int n = readInt();
+		int max = 0;
+		long ans = 0;
+		for (int i = 0; i < n; i++) {
+			int x = readInt();
+			if (x > max) {
+				max = x;
+			} else {
+				ans += max - x + 1;
+				max++;
+			}
+		}
+		System.out.println(ans);
+	}
+	static String next () throws IOException {
+		while (st == null || !st.hasMoreTokens())
+			st = new StringTokenizer(br.readLine().trim());
+		return st.nextToken();
+	}
+	static long readLong () throws IOException {
+		return Long.parseLong(next());
+	}
+	static int readInt () throws IOException {
+		return Integer.parseInt(next());
+	}
+	static double readDouble () throws IOException {
+		return Double.parseDouble(next());
+	}
+	static char readCharacter () throws IOException {
+		return next().charAt(0);
+	}
+	static String readLine () throws IOException {
+		return br.readLine().trim();
+	}
+}
